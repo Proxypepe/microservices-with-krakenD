6939fcaea847c1257df60dfc296c1850e08730f8
Proxypepe <sasha_golovin_95@bk.ru>
2022-12-06 22:57:18 +0300

    added tests

---
+++ product_service/test_service.py
@@ -0,0 +1,24 @@
+import unittest
+from mongoengine import connect, disconnect
+import httpx
+import pytest
+from main import app
+from httpx import AsyncClient
+
+
+class TestEndpoints(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        connect('mongoenginetest', host='mongomock://localhost')
+
+    @classmethod
+    def tearDownClass(cls):
+        disconnect()
+
+    async def test_get_all(self):
+        async with AsyncClient(app=app, base_url="http://test") as ac:
+            resource = await client.get('/v1/products')
+            assert resource.status_code == 200
+
+
